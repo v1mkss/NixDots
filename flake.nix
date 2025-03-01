897597a57764e08c1b9d548747ded76d4688efd5
v1mkss <v1mkss.m+git@gmail.com>
2025-03-01 19:38:22 +0200

    Refactor

--- flake.nix
+++ flake.nix
@@ -9,7 +9,13 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      ...
+    }@inputs:
     let
       system = "x86_64-linux";
       defaultUser = "v1mkss";
@@ -18,18 +24,20 @@
         inherit system;
         config.allowUnfree = true;
       };
-      
+
       # Function to get the list of users from /home directory
-      getUsers = let
-        readDir = dir: builtins.readDir dir;
-        isDirectory = type: type == "directory";
-        filterDirectories = dir: 
-          let contents = readDir dir;
-          in builtins.filter (name: isDirectory contents.${name}) (builtins.attrNames contents);
-      in
-        if builtins.pathExists /home
-        then filterDirectories /home
-        else [ defaultUser ];
+      getUsers =
+        let
+          readDir = dir: builtins.readDir dir;
+          isDirectory = type: type == "directory";
+          filterDirectories =
+            dir:
+            let
+              contents = readDir dir;
+            in
+            builtins.filter (name: isDirectory contents.${name}) (builtins.attrNames contents);
+        in
+        if builtins.pathExists /home then filterDirectories /home else [ defaultUser ];
     in
     {
       nixosConfigurations.v1mkss = nixpkgs.lib.nixosSystem {
@@ -57,15 +65,16 @@
                 # Pass the list of users to home.nix
                 users = getUsers;
               };
-              users = let
-                homeDirUsers = getUsers;
-                # Create configuration for each user from /home
-                mkUserConfig = user: {
-                  ${user} = import ./hosts/v1mkss/home.nix;
-                };
-              in
+              users =
+                let
+                  homeDirUsers = getUsers;
+                  # Create configuration for each user from /home
+                  mkUserConfig = user: {
+                    ${user} = import ./hosts/v1mkss/home.nix;
+                  };
+                in
                 # Merge all user configurations
-                builtins.foldl' (acc: user: acc // mkUserConfig user) {} homeDirUsers;
+                builtins.foldl' (acc: user: acc // mkUserConfig user) { } homeDirUsers;
             };
           }
         ];
