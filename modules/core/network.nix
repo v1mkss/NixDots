524425bc98e16ee6effe7d6216db682b6e53d8e3
v1mkss <v1mkss.m+git@gmail.com>
2025-03-01 19:38:39 +0200

    Firewall option added

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -6,6 +6,9 @@
   networking.networkmanager.enable = true;
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
+  # Enable Firewall
+  networking.firewall.enable = true;
+
   # Set your time zone.
   time.timeZone = "Europe/Kyiv";
 }
