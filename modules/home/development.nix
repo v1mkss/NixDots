5e0197c75fafda0695366bce4eafba17b3c123d5
Volodia Kraplich <v1mkss.m+git@gmail.com>
2025-03-29 14:14:20 +0200

    Switch to master nixpkgs channel

--- modules/home/development.nix
+++ modules/home/development.nix
@@ -6,6 +6,7 @@
     godot_4 # Game development engine
     lazygit # Terminal UI for git
     blender # 3D creation suite
+    androidStudioPackages.dev # Android Studio
 
     # Runtime and SDK
     bun # Fast JavaScript runtime/toolkit
